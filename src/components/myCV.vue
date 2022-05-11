52bf49aefaf783b1cd490f433623219b71cdb4bd
Enrico Carruba <carrubaenrico@gmail.com>
2022-05-11 16:13:12 +0200

    finished cv component

--- src/components/myCV.vue
+++ src/components/myCV.vue
@@ -11,24 +11,37 @@
                             <h3>{{      experiences[0].title    }}</h3>
                             <span> {{   experiences[0].year   }}</span>
                             <p> {{      experiences[0].content  }} </p>
-                            <button id="btn-article1" @click="expandArticle1 = !expandSection1, toggleBtnText($event)">More details</button>
+                            <button id="btn-article1" @click="expandArticle1 = !expandArticle1, toggleBtnText($event)">More details</button>
                                 <div class="table-container" v-show="expandArticle1" v-for="experience in experiences[0].experiences" :key="experience.id">
-                                        <h3 >{{ experience.year }}</h3> 
-                                        <p> {{  experience.position }}</p>   
+                                        <p >{{ experience.year }}</p> 
+                                        <p id="position"> {{  experience.position }}</p>   
                                         <p> {{  experience.company }}</p>
                                 </div>
+                            <hr>
                         </article>
                         <article>
                             <h3>{{      experiences[1].title    }}</h3>
                             <span> {{   experiences[1].year   }}</span>
                             <p> {{      experiences[1].content  }} </p>
-                            <button id="btn-article2" @click="expandArticle2 = !expandSection1, toggleBtnText($event)">More details</button>
+                            <button id="btn-article2" @click="expandArticle2 = !expandArticle2, toggleBtnText($event)">More details</button>
+                                <div class="table-container" v-show="expandArticle2" v-for="experience in experiences[1].experiences" :key="experience.id">
+                                        <p >{{ experience.year }}</p> 
+                                        <p id="position"> {{  experience.position }}</p>   
+                                        <p> {{  experience.company }}</p>
+                                </div>
+                            <hr>
                         </article>
                         <article>
                             <h3>{{      experiences[2].title    }}</h3>
                             <span> {{   experiences[2].year   }}</span>
                             <p> {{      experiences[2].content  }} </p>
-                            <button id="btn-article3" @click="expandArticle3 = !expandSection1, toggleBtnText($event)">More details</button>
+                            <button id="btn-article3" @click="expandArticle3 = !expandArticle3, toggleBtnText($event)">More details</button>
+                                <div class="table-container" v-show="expandArticle3" v-for="experience in experiences[2].experiences" :key="experience.id">
+                                        <p >{{ experience.year }}</p> 
+                                        <p id="position"> {{  experience.position }}</p>   
+                                        <p> {{  experience.company }}</p>
+                                </div>
+                            <hr>
                         </article>
                         
             
@@ -84,4 +97,28 @@ h3 {
     padding-top: 1rem;
     color: gray;
 }
+
+article h3{
+    color: black;
+    margin: 0 0 0 1rem;
+}
+
+article span {
+    margin: 0 0 1rem 1rem;
+    font-size: 0.75rem;
+}
+
+.table-container{
+    border: 1px solid lightblue;
+}
+
+.table-container p{
+    font-weight: 300;
+    margin: 0.5rem 1rem 0.5rem 1rem;
+}
+
+#position {
+    font-weight: bolder;
+}
+
 </style>
